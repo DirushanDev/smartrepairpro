e2a84c48edaf41082f23d406de797898a13e1acd
Rubakumar Dirushan <dirushan@amplifyd.com.au>
2025-08-11 19:51:55 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import About from "./components/About"
-import Contact from "./components/Contact"
+
 import Expertise from "./components/Expertise"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 import Heritage from "./components/Heritage"
-import InfoSection from "./components/InfoSection"
+
 import Journey from "./components/Journey"
-import Menu from "./components/Menu"
+
 import Navbar from "./components/Navbar"
 import Story from "./components/Story"
-import Testimonials from "./components/Testimonials"
 
 
 function App() {
