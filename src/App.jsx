cf5f0a0aaf1c2c075dff53ae1d87f034a9fe038a
Rubakumar Dirushan <dirushan@amplifyd.com.au>
2025-08-12 20:36:54 +0530

    add dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,31 @@
-
-import Wash from "./components/Expertise"
-import Footer from "./components/Footer"
-import Header from "./components/Header"
-import Service from "./components/Heritage"
-
-import Qa from "./components/Journey"
-
-import Navbar from "./components/Navbar"
-import Call from "./components/Story"
+// App.jsx
+import Wash from "./components/Expertise";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Service from "./components/Heritage";
+import Qa from "./components/Journey";
+import Navbar from "./components/Navbar";
+import Call from "./components/Story";
 import TestimonialSection from "./components/TestimonialSection";
+import ThemeToggle from "./components/ThemeToggle";
 
 function App() {
-
   return (
-    <>
+    <div className="min-h-screen bg-white text-gray-900 dark:bg-neutral-900 dark:text-gray-100">
       <Navbar />
+      <div className="container mx-auto px-4 py-3 flex justify-end">
+        <ThemeToggle />
+      </div>
+
       <Header />
-       <Service />
-       <Qa />
+      <Service />
+      <Qa />
       <Call />
-       <Wash imageSrc="/wash.png"  />
-       <TestimonialSection />
+      <Wash imageSrc="/wash.png" />
+      <TestimonialSection />
       <Footer />
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
